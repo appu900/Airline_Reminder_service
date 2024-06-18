64c515e1bdf69c54c58f52af0747e9de5b329872
pabitra <89703515+appu900@users.noreply.github.com>
2024-06-18 16:22:23 +0530

    added notification model

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ const { PORT } = require("./config/serverConfig");
 
 const sendBasicEmail = require("./services/email-service");
 
+var cron = require("node-cron");
+
 const setUpAndStartServer = async () => {
   const app = express();
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    
     // sendBasicEmail(
     //   "pabitrasundardakua@gmail.com",
     //   "pabitradakua85@gmail.com",
     //   "this is a testing email",
     //   "ur application is approved"
     // );
+
+    // cron.schedule("*/2 * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
+
   });
 };
 
